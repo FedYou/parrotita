013dea998e485746b139d2fbf1e005c2fe1f2f61
FedYou <federicochurruarin0@gmail.com>
2025-07-16 01:47:42 -0300

    feat: add functions to check data types

---
+++ utils/typeof.js
@@ -0,0 +1,64 @@
+const _typeof = {
+  string: isString,
+  number: isNumber,
+  boolean: isBoolean,
+  null: isNull,
+  integer: isInteger,
+  decimal: isDecimal,
+  array: isArray,
+  object: isObject
+}
+
+function isString(value) {
+  return typeof value === 'string'
+}
+function isBoolean(value) {
+  return typeof value === 'boolean'
+}
+
+function isNumber(value) {
+  return typeof value === 'number'
+}
+
+function isDecimal(value) {
+  return !Number.isInteger(value)
+}
+
+function isInteger(value) {
+  return Number.isInteger(value)
+}
+
+function isObject(value) {
+  return typeof value === 'object' && value !== null && !Array.isArray(value)
+}
+
+function isArray(value) {
+  return Array.isArray(value)
+}
+
+function isFunc(value) {
+  return typeof value === 'function'
+}
+
+function isUndefined(value) {
+  return value === undefined
+}
+
+function isNull(value) {
+  return value === null
+}
+
+export {
+  isString,
+  isBoolean,
+  isNumber,
+  isDecimal,
+  isInteger,
+  isObject,
+  isArray,
+  isFunc,
+  isUndefined,
+  isNull
+}
+
+export default _typeof
