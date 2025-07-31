60449b43b14985bedfc7e391f363f84f74fd1e81
FedYou <federicochurruarin0@gmail.com>
2025-07-31 02:06:16 -0300

    build: add script and dependencies for bundling

---
+++ build.js
@@ -0,0 +1,40 @@
+import esbuild from 'esbuild'
+import youfile from 'youfile'
+import { join } from 'path'
+
+const _dist = 'dist'
+const _package = 'package.json'
+const _externals = ['LICENSE', 'README.md']
+
+function buildScript(format) {
+  esbuild.build({
+    entryPoints: ['src/main.js'],
+    bundle: true,
+    minify: true,
+    target: 'es2015',
+    outfile: `${_dist}/lib/${format}.js`,
+    platform: 'neutral',
+    format
+  })
+}
+
+function buildPackage() {
+  const packageJson = youfile.read.json5Sync(_package)
+  delete packageJson.devDependencies
+  youfile.write.json(`${_dist}/package.json`, packageJson)
+}
+
+function copyExternals() {
+  for (const path of _externals) {
+    if (youfile.existsSync(path)) {
+      youfile.copySync(path, join(_dist, path))
+    }
+  }
+}
+
+youfile.removeExistsSync(_dist)
+
+buildScript('esm')
+buildScript('cjs')
+copyExternals()
+buildPackage()
