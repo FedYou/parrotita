b1375bf7dcfba27564ef9d36051359326d5e5d24
FedYou <federicochurruarin0@gmail.com>
2025-08-16 14:43:14 -0300

    chore: add type definitions for the project

---
+++ types/main.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Parrotita Validator
+ */
+
+declare class Validator {
+  constructor(schema: object | string, root: any)
+}
+
+export { Validator }
+
+declare const _default: {
+  Validator: typeof Validator
+}
+
+export default _default
