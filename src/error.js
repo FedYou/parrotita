01409cc80c652bbcb495edc44a893c13a8658276
FedYou <federicochurruarin0@gmail.com>
2025-08-24 00:29:12 -0300

    fix(resolveUnexpectedKey): prevent crash from premature serialized path

--- src/error.js
+++ src/error.js
@@ -42,7 +42,7 @@ function resolveUnexpectedKey(array) {
     const unexpectedKey = array[array.length - 1]
     array.pop()
     return {
-      path: serializeDotPath(array),
+      path: array,
       unexpectedKey: unexpectedKey
     }
   }
